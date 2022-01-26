073ac9bf2fe3f2e98e2a064727db6494ba4391fe
Vance Clark <02vance.clark@gulllakecs.org>
2022-01-26 09:36:24 -0800

    Initial Haunted Mansion Commit

---
+++ HauntedMansion/HauntedMansionProgram/HauntedMansion.js
@@ -0,0 +1,6 @@
+/*
+Programmer: Vance Clark
+Team Member: Aidan Howard
+Date: 1.26.2022
+Program: We will be making a program to go through a Haunted Mansion Maze filled with Haunted Sounds, Changing of colors, and other obstacles along the way using a Sphero Bolt and Java Script
+*/
