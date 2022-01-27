0de033c0a5bdb5109ecffde20b3c62c6b599f1e6
Vance Clark <02vance.clark@gulllakecs.org>
2022-01-27 09:57:09 -0800

    Goes to Sound Checkpoint - Vance Clark

--- HauntedMansion/HauntedMansionProgram/HauntedMansion.js
+++ HauntedMansion/HauntedMansionProgram/HauntedMansion.js
@@ -7,9 +7,20 @@ Program: We will be making a program to go through a Haunted Mansion Maze filled
 
 async function startProgram() {
 	// Write code here
+	// Says start and rolls the first stretch
+
 	await speak('Start', true)
 	await roll(0,60,2);
+	
+	// Set LED blue and goes to sound checkpoint 
+	
 	setMainLed({ r: 0, g: 0, b: 225 })
+	await roll(50, 30, 2)
+	await roll(90, 30, 2)
+	
+	// Plays monkey sound and goes to red checkpoint
+
+	await Sound.Animal.Monkey.play(true)
 	
 }
 
