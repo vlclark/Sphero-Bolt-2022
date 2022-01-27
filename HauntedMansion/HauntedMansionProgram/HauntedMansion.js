7d1b5e1db08dfaa3ca5b67a7a67f50f556711136
Vance Clark <02vance.clark@gulllakecs.org>
2022-01-27 09:31:34 -0800

    First Stretch - Vance Clark

--- HauntedMansion/HauntedMansionProgram/HauntedMansion.js
+++ HauntedMansion/HauntedMansionProgram/HauntedMansion.js
@@ -4,3 +4,9 @@ Team Member: Aidan Howard
 Date: 1.26.2022
 Program: We will be making a program to go through a Haunted Mansion Maze filled with Haunted Sounds, Changing of colors, and other obstacles along the way using a Sphero Bolt and Java Script
 */
+
+async function startProgram() {
+	// Write code here
+	await roll(0,60,2);
+}
+
