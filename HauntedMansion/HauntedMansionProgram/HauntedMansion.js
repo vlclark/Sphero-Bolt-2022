7a8d8b6e317a6014f4f9b5cf36c98e205d61e400
Vance Clark <02vance.clark@gulllakecs.org>
2022-02-01 08:57:04 -0800

    Red Check Point - Aidan Howard

--- HauntedMansion/HauntedMansionProgram/HauntedMansion.js
+++ HauntedMansion/HauntedMansionProgram/HauntedMansion.js
@@ -8,19 +8,22 @@ Program: We will be making a program to go through a Haunted Mansion Maze filled
 async function startProgram() {
 	// Write code here
 	// Says start and rolls the first stretch
-
+	
 	await speak('Start', true)
 	await roll(0,60,2);
 	
 	// Set LED blue and goes to sound checkpoint 
 	
-	setMainLed({ r: 0, g: 0, b: 225 })
+	setMainLed({ r: 0, g: 0, b: 255 })
 	await roll(50, 30, 2)
 	await roll(90, 30, 2)
 	
 	// Plays monkey sound and goes to red checkpoint
-
+	
 	await Sound.Animal.Monkey.play(true)
+	await roll(25,-29,2);
+	setMainLedainled({ r:255, g: 0, b: 0})
+	await roll(75,29,3);
 
 	
 }
